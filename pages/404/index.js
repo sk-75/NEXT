98c280185cc21490017441ec11fdcc9d075f86ed
Admin <l215323@lhr.nu.edu.pk>
2024-10-27 23:51:11 +0500

    committed

--- pages/404/index.js
+++
@@ -1,9 +0,0 @@
-
-export default function Custom404() {
-  return (
-    <div >
-      <h1>Oops! Page Not Found</h1>
-      <p>It seems like the page you are looking for doesn't exist.</p>
-    </div>
-  );
-}
